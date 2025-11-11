483dcaa5cf91de2fd7342f61a6139fd7be9841da
Ansh Kumar <anshagroia@gmail.com>
2025-11-12 00:07:21 +0530

    updated appContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.BACKEND_URL || "http://localhost:3000"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
 
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [user,setUser] = useState(null)
